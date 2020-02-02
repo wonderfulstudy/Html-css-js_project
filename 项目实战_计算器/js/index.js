68d6931b9969bf55779b6e6098752e871e9e1710
JYF <jiangyifeng2016@gmail.com>
2020-02-02 09:09:00 +0800

    第一次提交

---
+++ 项目实战_计算器/js/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function main(number01, number02, operate){
+    if(operate=="+"){
+
+    }else if(operate=="-"){
+
+    }else if(operate=="*"){
+
+    }else if(operate=="/"){
+
+    }else{
+
+    }
+}
+
+function 
